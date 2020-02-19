c7dee3e4b9aaa730000e474dc23594ad0db29bf2
moqiaoduo <415372363@qq.com>
2020-02-19 18:14:09 +0800

    删除无用助手函数

--- src/helpers.php
+++ src/helpers.php
@@ -22,10 +22,3 @@ if (!function_exists('getOption'))
         return $default;
     }
 }
-
-if (!function_exists('unCamelize')) {
-    function unCamelize($camelCaps,$separator='_')
-    {
-        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
-    }
-}
