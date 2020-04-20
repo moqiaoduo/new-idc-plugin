6516977492ab390375fb9107c4482317e8908b4d
moqiaoduo <415372363@qq.com>
2020-04-20 17:37:42 +0800

    更新触发机制

--- src/Manager.php
+++ src/Manager.php
@@ -2,8 +2,6 @@
 
 namespace NewIDC\Plugin;
 
-use Illuminate\Support\Arr;
-
 /**
  * 插件将遵循Laravel扩展包开发方法开发
  * 插件通过调用register方法注册hook
@@ -41,7 +39,7 @@ class Manager
 
     public function __construct()
     {
-        $this->ena_plugins=json_decode(getOption('ena_plugins'),true)?:[];
+        $this->ena_plugins = json_decode(getOption('ena_plugins'), true) ?: [];
     }
 
     /**
@@ -54,19 +52,20 @@ class Manager
     public function register(Plugin $plugin)
     {
         // 传入plugin对象，自动注册hook以及加入插件列表
-        $id=get_class($plugin);$this->plugins[$id]=$plugin->info();
-        if (($isServer=($plugin instanceof Server)) || ($ena=$this->checkEnable($id))) {
-            if (!($ena??false)) // 如果没有加入启用列表，则加入
-                $this->ena_plugins[]=$id;
-            if ($isServer) $this->server_plugins[]=$id;
-            foreach ((array) $plugin->hook() as $hook) {
-                $p=['plugin'=>$plugin];
+        $id = get_class($plugin);
+        $this->plugins[$id] = $plugin->info();
+        if (($isServer = ($plugin instanceof Server)) || ($ena = $this->checkEnable($id))) {
+            if (!($ena ?? false)) // 如果没有加入启用列表，则加入
+                $this->ena_plugins[] = $id;
+            if ($isServer) $this->server_plugins[] = $id;
+            foreach ((array)$plugin->hook() as $hook) {
+                $p = ['plugin' => $plugin];
                 if (isset($hook['func'])) {
-                    $p['func']=$hook['func'];
+                    $p['func'] = $hook['func'];
                 } else {
-                    $p['method']=$hook['method'];
+                    $p['method'] = $hook['method'];
                 }
-                $this->hooks[$hook['hook']][]=$p;
+                $this->hooks[$hook['hook']][] = $p;
             }
         }
     }
@@ -109,67 +108,30 @@ class Manager
      */
     public function checkEnable($id)
     {
-        return in_array($id,$this->ena_plugins);
+        return in_array($id, $this->ena_plugins);
     }
 
     /**
      * 触发钩子
+     * 结果以数组形式返回
      *
-     * @param array $params
+     * @param string $hookName
+     * @param mixed $data
+     * @param bool $hasRun
      * @return mixed
      */
-    public function trigger($params)
+    public function trigger($hookName, $data, &$hasRun)
     {
-        $default_params=[
-            'default_func'=>'',
-            'data'=>null,
-            'last'=>false,
-            'returnArray'=>false
-        ];
-        if (is_array($params)) {
-            $params=array_merge($default_params,$params);
-        } else {
-            $params=$default_params;
-            $params['hook']=$params;
-        }
-        $hasRun=false;$return=null;
-        $hooks=$this->hooks[$params['hook']]??[];
-        $data=$params['data'];
-        $returnArray=$params['returnArray'];
-        if ($returnArray) $return=[];
-        if ($params['last']) {
-            $hook=Arr::last($hooks);
-            $result=$this->singleRun($hook,$data,$hasRun);
-            if ($hasRun) {
-                if ($returnArray) $return=[$result];
-                else $return=$result;
-            }
-        } else {
-            foreach ((array) $hooks as $hook) {
-                $result=$this->singleRun($hook,$data,$hasRun);
-                if ($hasRun) {
-                    if ($returnArray) $return[]=$result;
-                    else $return.=$result;
-                }
+        $hooks = $this->hooks[$hookName] ?? [];
+        foreach ((array)$hooks as $hook) {
+            if (isset($hook['func']) && is_callable($hook['func'])) {
+                $hasRun = true;
+                $return[] = $hook['func']($data);
+            } elseif (isset($hook['method']) && is_callable([$hook['plugin'], $hook['method']])) {
+                $hasRun = true;
+                $return[] = $hook['plugin']->$hook['method']($data);
             }
         }
-        if (!$hasRun && is_callable($func=$params['default_func'])) {
-            $return=$func($data);
-            if ($returnArray) $return=[$return];
-        }
-        return $return;
-    }
-
-    protected function singleRun($hook, $data, &$hasRun)
-    {
-        if (isset($hook['func']) && is_callable($hook['func'])) {
-            $hasRun=true;
-            return $hook['func']($data);
-        }
-        if (isset($hook['method']) && is_callable([$hook['plugin'],$hook['method']])) {
-            $hasRun=true;
-            return $hook['func']($data);
-        }
-        return null;
+        return $return ?? [];
     }
 }
